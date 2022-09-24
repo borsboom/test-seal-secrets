2c990e5cb7ecb9bd7f4783465ac181ba751aa9a8
Emanuel Borsboom <eborsboom@fpcomplete.com>
2022-09-24 15:06:21 -0700

    Sat Sep 24 15:06:21 PDT 2022

--- scripts/update_secrets_from_github.py
+++ scripts/update_secrets_from_github.py
@@ -44,10 +44,10 @@ for secrets_map_path in secrets_map_paths:
             ], check=True)
             os.chdir(old_cwd)
             #@@@ CONSTANT FOR FILE SUFFIX
-            with open(f'{os.path.dirname(secrets_map_path)}/{secrets_map_row['sealedsecret_name']}_sealedsecret.yaml', 'r') as sealedsecret_file:
+            with open(f'{os.path.dirname(secrets_map_path)}/{secrets_map_row["sealedsecret_name"]}_sealedsecret.yaml', 'r') as sealedsecret_file:
                 sealedsecret_yaml = yaml.safe_load(sealedsecret_file)
             #@@@ COMPARE SHA TO NEW VALUE
             sealedsecret_yaml['metadata']['annotations']['update-secrets-from-github-sha256'] = "@@@ SETME"
             #@@@ CONSTANT FOR FILE SUFFIX
-            with open(f'{os.path.dirname(secrets_map_path)}/{secrets_map_row['sealedsecret_name']}_sealedsecret.yaml', 'w') as sealedsecret_file:
+            with open(f'{os.path.dirname(secrets_map_path)}/{secrets_map_row["sealedsecret_name"]}_sealedsecret.yaml', 'w') as sealedsecret_file:
                 yaml.dump(sealedsecret_yaml, sealedsecret_file)
